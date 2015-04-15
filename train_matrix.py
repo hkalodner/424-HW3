f1c71f4c5d6115b54a83df60cc7c91639bde76d9
Paul Ellenbogen <epaul9@cs.washington.edu>
2015-04-15 16:10:07 -0400

    remove ptrin statements

--- train_matrix.py
+++ train_matrix.py
@@ -7,7 +7,4 @@ train_data = np.load("txTripletsCounts.npy")
 dim = np.max(train_data) + 1
 matrix = csr_matrix((dim, dim), dtype = bool)
 
-print(len(train_data[:, 0]))
-print(matrix.shape)
 matrix[train_data[:, 0], train_data[:, 1]] = True
-

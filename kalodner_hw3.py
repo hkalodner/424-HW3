e1c7c3c440732edf2bd5bb0862005b1b6c28ef5b
Paul Ellenbogen <pe5@cs.princeton.edu>
2015-04-28 17:44:45 -0400

    ROC curves n stuff.

--- kalodner_hw3.py
+++ kalodner_hw3.py
@@ -1,3 +1,6 @@
+import pdb
+
+import matplotlib.pyplot as plt
 import numpy as np
 import scipy
 from scipy.sparse import csr_matrix, hstack, vstack
@@ -5,7 +8,7 @@ import graph_tool.all as gt
 from sklearn import cross_validation
 from sklearn import metrics
 
-from sklearn.metrics import classification_report
+from sklearn.metrics import classification_report, roc_curve, auc
 from sklearn.decomposition import TruncatedSVD
 from sklearn.svm import LinearSVC
 from sklearn.neighbors import KNeighborsClassifier
@@ -61,13 +64,19 @@ def predictForOutput(addr, relMatrix):
     vt_zero_columns = VarianceThreshold()
     train_x = vt_zero_columns.fit_transform(train_x, train_y)
     clf = MultinomialNB()
-    # clf = KNeighborsClassifier(n_neighbors=10)
+    # clf = KNeighborsClassifier(n_neighbors=5)
     # clf = LinearSVC(class_weight="auto")
     # clf = SGDClassifier(class_weight = "auto", loss = "squared_hinge")
     clf.fit(train_x, train_y)
     test_x = relMatrix[:, columnIndexes][fromAddresses, :]
     test_x = vt_zero_columns.transform(test_x)    
-    predicted = clf.predict(test_x)
+    # predicted = clf.predict(test_x)
+    predicted = clf.predict_proba(test_x)
+    if True in clf.classes_:
+        true_column = np.where(clf.classes_ == True)[0][0]
+        predicted = predicted[:, true_column]
+    else:
+        predicted = np.zeros(len(test_x))
     # test_y = test_set[test_set[:, 1] == addr][:, 2] >= 1
     # print(clf.score(test_x, test_y))
     # print(metrics.confusion_matrix(test_y, predicted))
@@ -75,62 +84,38 @@ def predictForOutput(addr, relMatrix):
     return predicted
 
 # print(predictForOutput(51, relMatrix))
-output_addresses = np.unique(test_set[:,1])
+# print(predictForOutput(239761, relMatrix))
 predictions = []
+output_addresses = np.unique(test_set[:,1])
 for i, address in enumerate(output_addresses):
     print("Computed for " + str(i))
     print("Address: " + str(address))
     predictions.append(predictForOutput(address, relMatrix))
 
-
-# predictions = np.load("multinomialNB_predictions.npy")
+    # predictions = np.load("multinomialNB_predictions.npy")
 predictions = np.hstack(predictions)
+# np.save("KNN_prob_predictions.npy", predictions)
+np.save("multinomialNB_prob_predictions.npy", predictions)
+# predictions = np.load("multinomialNB_prob_predictions.npy")
 
 actual = np.hstack([test_set[test_set[:, 1] == address][:, 2] >= 1
                     for address in output_addresses])
 
-# np.save("multinomialNB_predictions.npy", predictions)
-print(metrics.confusion_matrix(actual, predictions))
-print(classification_report(actual, predictions))
+# Compute ROC curve and area the curve
+fpr, tpr, thresholds = roc_curve(actual, predictions)
+roc_auc = auc(fpr, tpr)
 
-# eliminate column of 'to address'
-# eliminate rows of linked 'from addresses'
-
-# clf = LinearSVC(class_weight="auto").fit(relMatrix[:, indexes], relMatrix[:, 22506].toarray().ravel())
-
-# weight = graph.new_edge_property("int32_t")
-# for edge in graph.edges():
-#   weight[edge] = train_set[(train_set[:,0] == graph.vertex_index[edge.source()]) & (train_set[:,1] == graph.vertex_index[edge.target()]) ][0][2]
-
-# print("Calculated weights")
-
-# distanceList = []
-# reverseDistanceList = []
-# weightedDistanceList = []
-# for x in test_set:
-#   distanceList.append(distance(graph, x[0], x[1]))
-#   reverseDistanceList.append(reverseDistance(graph, x[0], x[1]))
-#   weightedDistanceList.append(weightedDistance(graph, weight, x[0], x[1]))
-
-# print("Calculated distanceData")
-
-# distanceArray = np.array(distanceList)
-# reverseArray = np.array(reverseDistanceList)
-# weightedArray = np.array(weightedDistanceList)
-# allDistance = np.transpose(np.vstack((distanceArray, reverseArray, weightedArray)))
-
-
-# X_train, X_test, y_train, y_test = cross_validation.train_test_split(allDistance, test_set[:,2], test_size=0.4, random_state=0)
-# clf = LinearSVC(class_weight="auto").fit(X_train, y_train)
-# clf.score(X_test, y_test)                           
-# clf = KNeighborsClassifier(n_neighbors=5).fit(X_train, y_train)
-
-# X_train, X_test, y_train, y_test = cross_validation.train_test_split(allDistance, test_set[:,2], test_size=0.4, random_state=2345624)
-# clf = SGDClassifier(class_weight="auto").fit(X_train, y_train)
-# clf.score(X_test, y_test)
-# predicted = clf.predict(X_test)
-# print(metrics.confusion_matrix(y_test, predicted))
-
+# Pltot ROC curve
+plt.clf()
+plt.plot(fpr, tpr, label='ROC curve (area = %0.2f)' % roc_auc)
+plt.plot([0, 1], [0, 1], 'k--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.0])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+# plt.title('Receiver operating characteristic example')
+plt.legend(loc="lower right")
+plt.show()
 
 
 
